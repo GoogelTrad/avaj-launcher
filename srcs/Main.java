c9a0621f9c212e72304e53aa67d01d14b628b5ea
GoogelTrad <celianmichez@free.fr>
2025-04-13 02:31:35 +0200

    uml finish, parse starting

--- srcs/Main.java
+++ srcs/Main.java
@@ -1,6 +1,8 @@
 package srcs;
 import java.io.File; 
-import java.io.FileNotFoundException; 
+import java.io.FileNotFoundException;
+import java.text.spi.NumberFormatProvider;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import aircraft.Baloon;
@@ -11,39 +13,88 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		Tower tower = new Tower();
-
-		Coordinates coord = new Coordinates(22, 33, 20);
-		Coordinates coord2 = new Coordinates(18, 27, 42);
-
-		WeatherTower weather = new WeatherTower();
-		Helicopter heli = new Helicopter(1, "coucou", coord);
-		JetPlane jet = new JetPlane(2, "prout", coord2);
-		Baloon bal = new Baloon(3, "oui", coord2);
-
-		tower.register(heli);
-		tower.register(jet);
-		tower.register(bal);
-
-		tower.conditionChange();
-		tower.unregister(heli);
-
-		// try
-		// {
-		// 	File file = new File(args[0]);
-		// 	Scanner read = new Scanner(file);
-		// 	String data = "";
-		// 	while(read.hasNextLine()) {
-		// 		data += read.nextLine();
-		// 		data += "\n";
-		// 	}
-		// 	System.out.print(data);
-		// 	read.close();
-		// }
-		// catch(FileNotFoundException e)
-		// {
-		// 	System.out.println("An error has occured.");
-		// 	e.printStackTrace();
-		// }
+		ArrayList<String> data = new ArrayList<String>();
+		try
+		{
+			File file = new File(args[0]);
+			Scanner read = new Scanner(file);
+			while(read.hasNextLine()) {
+				data.add(read.nextLine());
+			}
+			// System.out.print(data);
+			read.close();
+		}
+		catch(FileNotFoundException e)
+		{
+			System.out.println("An error has occured.");
+			e.printStackTrace();
+		}
+		for (String item : data) {
+            System.out.println(item);
+        }
 	}
+
+	public boolean check_scenario(ArrayList<String> data)
+	{
+		int cycle = 0;
+		if (data.size() >= 0)
+		{
+			try {
+				cycle = Integer.parseInt(data.getFirst());
+			}
+			catch (NumberFormatException e) {
+				System.out.println("Error : Intructions incorrect !");
+				return false;
+			}
+
+			if (cycle <= 0)
+			{
+				System.out.println("Error : First line is incorrect !");
+				return false;
+			}
+		}
+		for (String element : data) 
+		{
+			String[] split = element.split(" ");
+
+			for (int k = 0; k < split.length; k++) 
+			{
+				if (split.length >= 0 && !isClassExisting(split[0])) 
+				{
+					System.out.println("Error : " + split[0] + " is not an existant class !");
+					return false; 
+				}
+				for (int j = 2; j < 5; j++)
+				{
+					if (checkIfInteger(split, j) == false)
+						return false;
+				}
+			}
+		}
+		return true;
+	}
+
+
+	public static boolean isClassExisting(String className) {
+        try {
+            Class.forName(className);
+            return true;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
+    }
+
+	private static boolean checkIfInteger(String[] array, int index) {
+        if (index >= array.length || array[index] == null) {
+            return false;
+        }
+
+        try {
+            int value = Integer.parseInt(array[index]);
+        } catch (NumberFormatException e) {
+            System.out.println("Error : Coordinate number" + index + " is not valid !");
+			return false;
+        }
+		return true;
+    }
 }
--- srcs/aircraft/AircraftFactory.java
+++ srcs/aircraft/AircraftFactory.java
@@ -1,12 +1,55 @@
 package aircraft;
 
+import javax.naming.directory.NoSuchAttributeException;
+
 import aircraft.Flyable;
 import srcs.Coordinates;
 
 public class AircraftFactory {
 
+	private static long ids = 0;
+	private static AircraftFactory instance;
+
+	public static AircraftFactory getInstance()
+	{
+		if (instance == null)
+			instance = new AircraftFactory();
+
+		return instance;
+	}
+
+
 	public Flyable newAircraft(String p_type, String p_name, Coordinates p_coordinates)
 	{
+		try
+		{
+			if (p_coordinates == null || p_type == null || p_name == null)
+				throw new NoSuchAttributeException("Type, name and coordinates can't be null !");
+	
+			if (p_coordinates.getHeight() < 0 || p_coordinates.getLatitude() < 0 || p_coordinates.getLongitude() < 0)
+				throw new ArithmeticException("Error: Cannot have negatives coordinates !");
+	
+			if (p_coordinates.getHeight() > 100)
+				p_coordinates.setHeight(100);
+	
+			if (p_type.toLowerCase().equals("helicopter"))
+				return new Helicopter(ids++, p_name, p_coordinates);
+			else if (p_type.toLowerCase().equals("baloon"))
+				return new Baloon(ids++, p_name, p_coordinates);
+			else if (p_type.toLowerCase().equals("jetplane"))
+				return new JetPlane(ids++, p_name, p_coordinates);
+			else
+				return null;
+		}
+		catch(NoSuchAttributeException e)
+		{
+			e.printStackTrace();
+		}
+		catch(ArithmeticException e)
+		{
+			e.printStackTrace();
+		}
+
 		return null;
 	}
 }
